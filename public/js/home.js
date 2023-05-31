877fa3f5b40754013413d0b17158fce1fa11389d
raulgm21 <raulgomeeez21@gmail.com>
2023-05-31 17:00:31 +0200

    MODULOS PARA TODOS

--- public/js/home.js
+++ public/js/home.js
@@ -27,6 +27,75 @@
 
 
 // ----------------------------------------------------------------------------------------------- //
+
+    // ---------------------------------------------------------------------- //
+    // MÓDULO HORAS
+    // ---------------------------------------------------------------------- //
+
+    if(document.getElementById("MOD_HORA_ADQUIRIDO")){
+        
+        function imprimirHora() {
+            document.getElementById("HOME_HORA_ACTUAL").innerHTML = new Date().toLocaleTimeString();
+        }
+          
+        setInterval(imprimirHora, 1000);
+
+    }
+
+    // ---------------------------------------------------------------------- //
+    // MÓDULO CALENDARIO
+    // ---------------------------------------------------------------------- //
+
+    if(document.getElementById("MOD_CALENDARIO_ADQUIRIDO")){
+
+        function crearCalendario() {
+            // Variables
+            const hoy          = new Date();
+            const mes          = hoy.getMonth();
+            const ano          = hoy.getFullYear();
+            const primerDiaMes = new Date(ano, mes, 1);
+            const ultimoDiaMes = new Date(ano, mes + 1, 0);
+            const diasSemana   = ['Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa', 'Do'];
+            const meses        = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+          
+            // Mes y Año ACTUAL
+            let tabla = `<table><caption style="background : ${COLOR} ; color : white">${meses[mes]} ${ano}</caption><thead><tr>`;
+          
+            // Mostrar Días de la Semana
+            for (let i = 0; i < 7; i++) {
+                const diaIndex = i % 7;
+                tabla += `<th>${diasSemana[diaIndex]}</th>`;
+            }
+            tabla += '</tr></thead><tbody><tr>';
+          
+            for (let i = 1; i < primerDiaMes.getDay(); i++) {
+                tabla += '<td></td>';
+            }
+
+            for (let dia = 1; dia <= ultimoDiaMes.getDate(); dia++) {
+    
+                const fechaActual = new Date(ano, mes, dia);
+                
+                if (fechaActual.getDay() === 1 && dia !== 1) {
+                tabla += '</tr><tr>';
+                }
+    
+                if (dia === hoy.getDate()) {
+                    tabla += `<td id="HOME_CALENDARIO_HOY" style="background : ${COLOR}">${dia}</td>`;
+                    
+                }else {
+                    tabla += `<td>${dia}</td>`;
+                }
+            }
+          
+            document.getElementById('HOME_CALENDARIO').innerHTML = tabla;
+        }
+    
+        crearCalendario();
+
+    }
+
+// ----------------------------------------------------------------------------------------------- //
     
     // ---------------------------------------------------------------------- //
     // Sección de código que nos permite mostrar y ocultar el menú del header
